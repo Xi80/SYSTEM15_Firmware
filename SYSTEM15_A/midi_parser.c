3890718f85ca846729cb40f4bbc3f08a3e0cb139
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2022-01-22 00:21:07 +0900

    Add midi parser(in progress)

---
+++ SYSTEM15_A/midi_parser.c
@@ -0,0 +1,8 @@
+/*
+ * midi_parser.c
+ *
+ * Created: 2022/01/22 0:20:19
+ *  Author: Owner
+ */ 
+#include "midi_parser.h"
+
---
+++ SYSTEM15_A/midi_parser.h
@@ -0,0 +1,16 @@
+/*
+ * midi_parser.h
+ *
+ * Created: 2022/01/22 0:20:10
+ *  Author: Owner
+ */ 
+
+
+#ifndef MIDI_PARSER_H_
+#define MIDI_PARSER_H_
+
+
+
+
+
+#endif /* MIDI_PARSER_H_ */
