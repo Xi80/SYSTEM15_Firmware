59f824ec2356361d4710fcb998bcc1e49dc9e9a6
Xi80 <h31e26@ed.cc.suzuka-ct.ac.jp>
2022-01-22 00:39:10 +0900

    Update

--- SYSTEM15_A/midi_parser.c
+++ SYSTEM15_A/midi_parser.c
@@ -6,3 +6,33 @@
  */ 
 #include "midi_parser.h"
 
+/*構造体*/
+midi_msg_t msg;
+
+/*プライベート変数*/
+uint8_t work_midi[3];			/*ワークメモリ*/
+uint8_t ex_ptr;					/*エクスクルーシブ用ポインタ*/
+uint8_t flag;					/*フラグ(bit0:3バイト目フラグ/bit1:SysExフラグ)*/
+uint8_t running_status_buffer;	/*ランニングステータスバッファ*/
+uint8_t rpn[2];					/*RPN*/
+
+void init_midi_parser(void);{
+	/*各種メモリの初期化*/
+	memset(&work_midi,0x00,3);
+	memset(&rpn,0x00,2);
+	ex_ptr					= 0x00;
+	flag					= 0x00;
+	running_status_buffer	= 0x00;
+}
+
+void parse_midi_data(void);{
+	
+}
+
+void decode_midi_message(void);{
+	
+}
+
+void decode_exclusive(void);{
+	
+}
--- SYSTEM15_A/midi_parser.h
+++ SYSTEM15_A/midi_parser.h
@@ -9,8 +9,52 @@
 #ifndef MIDI_PARSER_H_
 #define MIDI_PARSER_H_
 
+#include <avr/io.h>
+#include <string.h>
 
+void init_midi_parser(void);		/*MIDIパーサの初期化*/
 
+void parse_midi_data(void);			/*MIDIデータのパース*/
+
+void decode_midi_message(void);		/*MIDIメッセージの解読*/
+
+void decode_exclusive(void);		/*SysExの解読*/
+
+/*マクロ*/
+#define MSG_SOX			0xF0
+#define MSG_EOX			0xF7
+
+#define MSG_NOTE_ON		0x90
+#define MSG_NOTE_OFF	0x80
+#define MSG_CC			0xB0
+#define MSG_PC			0xC0
+#define MSG_PB			0xE0
+
+#define CC_MOD			0x01;
+#define CC_DE_MSB		0x06;
+#define CC_VOL			0x07;
+#define CC_EXP			0x0B;
+#define CC_HOLD			0x40;
+#define CC_RPN_LSB		0x64;
+#define CC_RPN_MSB		0x65;
+
+#define CC_ASO			0x78
+#define CC_RAC			0x79
+#define CC_ANO			0x7B
+
+#define EX_GS			0x01;
+#define EX_88			0x02;
+#define EX_XG			0x03;
+#define EX_GM			0x04;
+
+/*構造体*/
+typedef struct {
+	uint8_t msg;
+	uint8_t ch;
+	uint8_t data[2];
+}midi_msg_t;
+
+/*グローバル変数*/
 
 
 #endif /* MIDI_PARSER_H_ */
